dfbd0a08a044445491b33ae19fd58003494dd24b
ialba75 <ialba@asu.edu>
2019-04-25 13:28:15 -0700

    fixed smote thing

--- Boosting.py
+++ Boosting.py
@@ -6,9 +6,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report
 from sklearn.decomposition import PCA
-import sklearn.svm
 import time
-from sklearn.linear_model import LogisticRegression
 import multiprocessing
 
 from imblearn.over_sampling import SMOTE
@@ -20,18 +18,18 @@ target_names = None
 use_PCA = False
 use_SMOTE = True
 smote_threshold = 1000
+output_folder = 'adaboost_outputs'
+resultsFile = output_folder + '/results'
+graphFile = output_folder + '/graphs'
 
 
 def preprocessing():
     global target_names
 
-    # data = helpers.FileReader.loadCsv('data/dataSet.csv')
-    # labels = helpers.FileReader.loadCsv('data/labels.csv')
-
     data = helpers.FileReader.loadCsv('data/dataSetMedium.csv')
     labels = helpers.FileReader.loadCsv('data/labelsMedium.csv')
 
-    labels.rename(columns={0: 'label'}, inplace=True)
+    labels.rename(columns={1: 'label'}, inplace=True)
     target_names = list(labels.label.value_counts().index)
     print("Unique labels: ", target_names)
     print(labels.label.value_counts())
@@ -65,10 +63,12 @@ def preprocessing():
         for k, v in smote_targets.items():
             if v < smote_threshold:
                 smote_targets[k] = smote_threshold
-        # print("smote targets: ", smote_targets)
+        print("smote targets: ", smote_targets)
 
         print('Original dataset shape %s' % Counter(Y))
-        sm = SMOTE(sampling_strategy=smote_targets, k_neighbors=20, random_state=42, n_jobs=multiprocessing.cpu_count() - 1)
+        sm = SMOTE(sampling_strategy=smote_targets, k_neighbors=5, random_state=42,
+                   n_jobs=multiprocessing.cpu_count() - 1)
+
         X_res, Y_res = sm.fit_resample(X, Y)
         print('Resampled dataset shape %s' % Counter(Y_res))
 
@@ -77,17 +77,18 @@ def preprocessing():
 
 def main(X, Y):
 
-    # model = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2),
-    #                            n_estimators=50,
-    #                            learning_rate=1)
-
-    model = sklearn.svm.SVC()# LogisticRegression(n_jobs=8)
-
-
     kf = KFold(n_splits=10, shuffle=True)
+    num_estimators = [10, 25, 50, 75, 100, 125, 150, 200]
+    count = 0
 
-    print("Starting KFold...")
+
     for train_idx, test_idx in kf.split(X):
+        curr_num = num_estimators[count]
+        print("Starting Fold... num_estimators=", curr_num)
+        model = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2),
+                                   n_estimators=50,
+                                   learning_rate=1)
+
         X_train, X_test = X[train_idx], X[test_idx]
         Y_train, Y_test = Y[train_idx], Y[test_idx]
         pred = model.fit(X_train, Y_train).predict(X_test)
